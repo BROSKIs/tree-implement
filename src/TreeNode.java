e859641ce0bd74faebd41154da361779b03ebfd8
Pasha <pavloyousee@gmail.com>
2026-05-28 11:49:40 -0700

    end of class commit

--- src/TreeNode.java
+++ src/TreeNode.java
@@ -3,5 +3,19 @@ public class TreeNode {
     public TreeNode left;
     public TreeNode right;
 
+    public TreeNode (int data){
+        this.data = data;
+    }
+
+    public TreeNode (int data, TreeNode left){
+        this.data = data;
+        this.left = left;
+    }
+    public TreeNode (int data, TreeNode left, TreeNode right){
+        this.data = data;
+        this.left = left;
+        this.right = right;
+    }
+
   
 }
